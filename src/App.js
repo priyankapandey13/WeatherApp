fcb1ea39287bff015d346e395a997cc6c41bf55e
Priyanka <priyanka.mydesire@gmail.com>
2024-04-22 12:58:15 +0200

    Dummy items : dropdown and submit button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Fun weather app</h1>
+        <form >
+          <select >
+            <option>Dropdown</option>
+            <option value="India">India</option>
+            <option value="Spain">Spain</option>
+            <option value="Denmark">Denmark</option>
+          </select>
+          <select >
+            <option>Dropdown</option>
+            <option value="City01">City01</option>
+            <option value="City02">City02</option>
+            <option value="City03">City03</option>
+          </select>
+          <button >Submit</button>
+        </form>
     </div>
   );
 }
